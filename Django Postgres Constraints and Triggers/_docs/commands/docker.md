10b199619e53f25965530811e4110146492c47f2
Zander <subscription@veryacademy.com>
2024-07-18 14:35:58 +0100

    nc

--- Django Postgres Constraints and Triggers/_docs/commands/docker.md
+++
@@ -1,8 +0,0 @@
-# Docker Commands
-
-This guide outlines various docker commands.
-
-- **Build and start docker**
-    ```
-    docker compose up
-    ```
--- Django Postgres Constraints and Triggers/django-inventory/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-def pytest_collection_modifyitems(items):
-    for item in items:
-        if "model" in item.name:
-            item.add_marker(pytest.mark.model)
-        if "structure" in item.name:
-            item.add_marker(pytest.mark.model_structure)
